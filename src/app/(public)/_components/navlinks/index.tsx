1620d713d66a691ae427dc665071b705aea8968e
Fábio Valentim <maitabom@gmail.com>
2025-11-08 17:55:44 -0300

    adiciona lógica de sessão e botão de login ao componente NavLinks

--- src/app/(public)/_components/navlinks/index.tsx
+++ src/app/(public)/_components/navlinks/index.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { NavLinksProperties } from "./properties";
+import { LogIn } from "lucide-react";
 
 type LinkItem = {
   href: string;
   label: string;
 };
+
+const session = null;
 
 const navItems: LinkItem[] = [
   { href: "#profissionais", label: "Profissionais" },
@@ -20,8 +23,16 @@ export const NavLinks = (properties: NavLinksProperties) => (
         className="bg-transparent hover:bg-transparent text-black shadow-none"
         asChild
       >
-        <Link href={item.href}>{item.label}</Link>
+        <Link className="text-base" href={item.href}>{item.label}</Link>
       </Button>
     ))}
+
+    {session ? (
+      <Link href="/dashboard" className="flex items-center gap-2">Painel da Clínica</Link>
+    ) : (
+      <Button>
+        <LogIn /> Login
+      </Button>
+    )}
   </>
 );
