840d81903b34cb73bc8425eb2de317f928803ed8
Fábio Valentim <maitabom@gmail.com>
2025-11-09 17:23:50 -0300

    adiciona componente Footer para rodapé da página

---
+++ src/app/(public)/_components/footer/index.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="py-6 text-center text-gray-500 text-sm md:text-base">
+      <p>&copy; Todos os direitos reservados.</p>
+    </footer>
+  );
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
 import { Professionals } from "./_components/professionals";
@@ -8,8 +9,9 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <Professionals/>
+        <Professionals />
       </main>
+      <Footer />
     </div>
   );
 }
