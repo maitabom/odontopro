6735fa6f6d71486e4d1d47730bc802c5f5104443
Fábio Valentim <maitabom@gmail.com>
2025-11-15 17:28:52 -0300

    adiciona componentes de layout para o painel de controle

---
+++ src/app/(panel)/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import { DashboardLayoutProperties } from "./layout.properties";
+
+export default function DashboardLayout({
+  children,
+}: DashboardLayoutProperties) {
+  return <>{children}</>;
+}
